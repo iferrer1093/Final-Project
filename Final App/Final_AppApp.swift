74d9e0de59c5f07066c441859a83f60e28631ed4
iferrer1093 <if0999762@otc.edu>
2026-05-04 08:18:14 -0500

    Initial Commit

---
+++ Final App/Final_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_AppApp.swift
+//  Final App
+//
+//  Created by FERRER, ISAAC on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Final_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
