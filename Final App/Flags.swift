8a2d21963353805530c50c01b385e7bea6cbdbdb
iferrer1093 <if0999762@otc.edu>
2026-05-06 09:27:30 -0500

    added flags file for detail view. dict junk is next.

---
+++ Final App/Flags.swift
@@ -0,0 +1,18 @@
+//
+//  Flags.swift
+//  Final App
+//
+//  Created by FERRER, ISAAC on 5/6/26.
+//
+
+import SwiftUI
+
+struct Flags: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Flags()
+}
